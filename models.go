472852007adc4953fd00c53a74164629dacdc534
Daniel Adeboye <oluwadaunsid@gmail.com>
2024-02-06 22:49:04 +0100

    updated models and database for the to include new fields

--- models.go
+++ models.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/daunsid/rssagg/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -11,4 +12,13 @@ type User struct {
 	CreateAt  time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(dbUser database.User) *User {
+	return &User{
+		ID:        dbUser.ID,
+		CreateAt:  dbUser.CreateAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+	}
 }
